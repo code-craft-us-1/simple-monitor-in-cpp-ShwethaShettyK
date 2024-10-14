1c6ef51fb36334b651bcfb9e60d0c1dc263d7d0c
Shwetha Shetty K <104064727+ShwethaShettyK@users.noreply.github.com>
2024-10-14 09:53:39 +0530

    Update test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -2,16 +2,19 @@
 
 #include "gtest/gtest.h"
 
-TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
+TEST(Monitor, TestLowOxygenSaturation) {
   vitalSigns vitalSign = { 99, 102, 70 };
   ASSERT_FALSE(vitalsOk(vitalSign));
-
+}
+TEST(Monitor, TestNormalVitals) {
   vitalSign = { 98.1, 70, 98 };
   ASSERT_TRUE(vitalsOk(vitalSign));
-
+}
+TEST(Monitor, TestLowPulseRate) {
   vitalSign = { 98.1, 55, 98 };
   ASSERT_FALSE(vitalsOk(vitalSign));
-
+}
+TEST(Monitor, TestHighTemperature) {
   vitalSign = { 103, 70, 98 };
   ASSERT_FALSE(vitalsOk(vitalSign));
 }
