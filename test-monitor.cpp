00cc1e3fe3658e028974a8b60c321aaeeded7530
Shwetha Shetty K <104064727+ShwethaShettyK@users.noreply.github.com>
2024-10-14 12:27:11 +0530

    Update test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -2,16 +2,9 @@
 
 #include "gtest/gtest.h"
 
-TEST(Monitor, TestLowOxygenSaturation) {
+TEST(Monitor, TestVitalsOk) {
     ASSERT_FALSE(vitalsOk({ 99, 102, 70 }));
-}
-TEST(Monitor, TestNormalVitals) {
     ASSERT_TRUE(vitalsOk({ 98.1, 70, 98 }));
-}
-TEST(Monitor, TestLowPulseRate) {
     ASSERT_FALSE(vitalsOk({ 98.1, 55, 98 }));
-}
-TEST(Monitor, TestHighTemperature) {
     ASSERT_FALSE(vitalsOk({ 103, 70, 98 }));
 }
-
